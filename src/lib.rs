e74138ff7ccd89cce87c74284b90e7fd1a8831e2
Tanmoy Paul <10973676+paultanmoy@users.noreply.github.com>
2024-09-10 02:02:12 +0530

    impl Display for Message

--- src/lib.rs
+++ src/lib.rs
@@ -1,3 +1,6 @@
+use std::fmt;
+
+use base64::prelude::{BASE64_STANDARD, Engine as _};
 use serde::Serialize;
 
 #[derive(Clone, Debug, Serialize)]
@@ -6,29 +9,48 @@ pub struct Image {
     data: Vec<u8>,
 }
 
+impl fmt::Display for Image {
+    #[inline]
+    fn fmt(&self, f: &mut fmt::Formatter<'_>) -> fmt::Result {
+        f.write_fmt(format_args!("data:{};base64, {}", self.media_type(), BASE64_STANDARD.encode(self.data())))
+    }
+}
+
 impl Image {
     #[inline]
     pub fn new(media_type: impl Into<String>, data: Vec<u8>) -> Self {
         Self { media_type: media_type.into(), data }
     }
 
+    #[inline]
     pub fn media_type(&self) -> &str {
         &self.media_type
     }
 
+    #[inline]
     pub fn data(&self) -> Vec<u8> {
         self.data.clone()
     }
 }
 
-#[derive(Clone, Serialize)]
+#[derive(Clone, Debug, Serialize)]
 #[serde(tag = "type")]
 pub enum Message {
-    #[serde(rename = "text")]
-    Text { text: String },
-
     #[serde(rename = "image")]
     Image(Image),
+
+    #[serde(rename = "text")]
+    Text { text: String },
+}
+
+impl fmt::Display for Message {
+    #[inline]
+    fn fmt(&self, f: &mut fmt::Formatter<'_>) -> fmt::Result {
+        match self {
+            Message::Image(image) => f.write_fmt(format_args!("{}", image)),
+            Message::Text { text } => f.write_str(text.as_str()),
+        }
+    }
 }
 
 mod assistant;
